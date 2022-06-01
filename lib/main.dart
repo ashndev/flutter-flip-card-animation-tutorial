b338f66058dca7ae82484caf94801d9706ad58ff
ashndev <87834445+ashndev@users.noreply.github.com>
2022-05-31 18:59:03 -0700

    Finished Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,56 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const AppWidget());
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  late FlipCardController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = FlipCardController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Flip Card Animation Tutorial',
+      title: 'Material App',
       home: Scaffold(
         backgroundColor: const Color.fromARGB(172, 81, 187, 177),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Image(
-                image: AssetImage('assets/images/coin-front.png'),
-                width: 200,
+              FlipCard(
+                controller: _controller,
+                back: const Image(
+                  image: AssetImage('assets/images/coin-back.png'),
+                  width: 200,
+                ),
+                front: const Image(
+                  image: AssetImage('assets/images/coin-front.png'),
+                  width: 200,
+                ),
               ),
               const SizedBox(
                 height: 20,
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (!_controller.state!.isFront) {
+                    _controller.toggleCard();
+                  }
+                },
                 icon: const Icon(Icons.threesixty_rounded),
                 iconSize: 70,
                 color: const Color.fromARGB(255, 255, 255, 255),
